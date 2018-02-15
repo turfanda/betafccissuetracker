3782fd57715d0c617deedf1c2464a67a99e7b1f1
Glitch (betafcc-issuetracker) <none>
2018-02-15 10:08:14 +0000

    🏖🏈 Checkpoint server.js:596315/99 package.json:596315/19 .env:596315/1 views/index.html:596315/7 public/style.css:596315/3 public/client.js:596315/1

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
+var mongoose = require("mongoose");
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+
+mongoose.connect(process.env.MONGO_URL, function(err){
+    if(err) {
+        console.log('Some problem with the connection ' +err);
+    } else {
+        console.log('The Mongoose connection is ready');
+    }
+});
+
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
