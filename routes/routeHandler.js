f89db3f99d8653ba2108bbed5f1744aa8e3bf7cf
Glitch (betafcc-issuetracker) <none>
2018-02-16 12:53:16 +0000

    🎣👭 Checkpoint routes/routeHandler.js:596315/310

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -24,21 +24,20 @@ exports.getAllProject = function(req, res) {
     });
 }
 
-
 exports.createIssue = function(req, res) {
-    
+        console.log(0)
   let projectId;
-      projectModel.find({project_name:req.params.projectName},function(err, data) {
-        if (err)
-          console.log(err);
+projectModel.findOne({"project_name":req.params.projectName},function(err, data) {
+          console.log(1)
+        if (err){
+    let project = new projectModel({
+        project_name: req.params.projectName
+    });
+    project.save(function(err, data) {
+        if (err) console.log(err);
         else {
             projectId=data._id;
-        }
-    });
-console.log(projectId)
-  console.log(1)
-  
-      let issue = new issueModel({
+                          let issue = new issueModel({
   issue_title: req.body.issueName,
 	issue_text: req.body.issueText,
 	created_by: req.body.createdBy,
@@ -53,10 +52,36 @@ console.log(projectId)
     issue.save(function(err, data) {
         if (err) console.log(err);
         else {
-          console.log(1)
-          console.log(data);
             return res.json(data);
         }
     });
+        }
+    });
+        
+        }
+
+        else {
+            projectId=data._id;
+                let issue = new issueModel({
+  issue_title: req.body.issueName,
+	issue_text: req.body.issueText,
+	created_by: req.body.createdBy,
+  assigned_to:req.body.assignedTo,
+	created_on: Date().now,
+  updated_on:Date().now,
+  isOpen:true,
+  status:req.body.status,
+        _project:projectId
+    });
+  
+    issue.save(function(err, data) {
+        if (err) console.log(err);
+        else {
+            return res.json(data);
+        }
+    });
+        }
+    });
+
 }
 
