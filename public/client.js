8b6ee4ad2eb5dc178c0976fadb796fc907f4674a
Glitch (betafcc-issuetracker) <none>
2018-02-18 06:35:20 +0000

    🎍🤒 Checkpoint public/client.js:596315/115 views/index.html:596315/47 routes/routeHandler.js:596315/8 models/issue.js:596315/1

--- public/client.js
+++ public/client.js
@@ -1,72 +1,72 @@
 $(function() {
     $("#issueSubmit").on("click", function(e) {
-          e.preventDefault();
+        e.preventDefault();
         var url = "api/issues/" + $("#pname").val();
         $.post(url, $('#issueCreateForm').serialize(), function(data) {
             console.log(data);
-           $("input").val("");
+            $("input").val("");
         });
 
     });
-      $("#issueDeleteSubmit").on("click", function(e) {
-          e.preventDefault();
-        var url = "api/issues/"+$(this).prevAll().eq(1).val();
-              $.ajax({
-            type:"delete",
-            url:url,
-            data:$('#issueDeleteForm').serialize(),
-            success: function(response){
-              $("input").val("");
-               $(".resultDiv").empty();
-              $(".resultDiv").append($("<span>").text(response));
+    $("#issueDeleteSubmit").on("click", function(e) {
+        e.preventDefault();
+        var url = "api/issues/" + $(this).prevAll().eq(1).val();
+        $.ajax({
+            type: "delete",
+            url: url,
+            data: $('#issueDeleteForm').serialize(),
+            success: function(response) {
+                $("input").val("");
+                $(".resultDiv").empty();
+                $(".resultDiv").append($("<span>").text(response));
             },
-                            error:function(err){
-              $(".resultDiv").empty();
-               $(".resultDiv").append(err.responseText);
+            error: function(err) {
+                $(".resultDiv").empty();
+                $(".resultDiv").append(err.responseText);
             }
         });
-        
+
 
 
     });
-  
-        $("#issueUpdateForm").submit(function(e) {
 
-          e.preventDefault();
-        var url = "api/issues/"+$(this).prevAll().eq(8).val();
-              $.ajax({
-            type:"put",
-            url:url,
-            data:$('#issueUpdateForm').serialize(),
-            success: function(response){
-              $("input").val("");
-               $(".resultDiv").empty();
-              $(".resultDiv").append($("<span>").text(response));
+    $("#issueUpdateForm").submit(function(e) {
+
+        e.preventDefault();
+        var url = "api/issues/" + $(this).children().val();;
+        $.ajax({
+            type: "put",
+            url: url,
+            data: $('#issueUpdateForm').serialize(),
+            success: function(response) {
+                $("input").val("");
+                $(".resultDiv").empty();
+                $(".resultDiv").append($("<span>").text(response));
             },
-                            error:function(err){
-              $(".resultDiv").empty();
-               $(".resultDiv").append(err.responseText);
+            error: function(err) {
+                $(".resultDiv").empty();
+                $(".resultDiv").append(err.responseText);
             }
         });
-        
+
 
 
     });
 
     $("#projectSubmit").on("click", function(e) {
-      e.preventDefault();
-      $.ajax({
-            type:"post",
-            url:"api/",
-            data:$('#projectCreateForm').serialize(),
-            success: function(response){
-              $("input").val("");
-               $(".resultDiv").empty();
-              $(".resultDiv").append($("<span>").text(response));
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "api/",
+            data: $('#projectCreateForm').serialize(),
+            success: function(response) {
+                $("input").val("");
+                $(".resultDiv").empty();
+                $(".resultDiv").append($("<span>").text(response));
             },
-                    error:function(err){
-              $(".resultDiv").empty();
-               $(".resultDiv").append(err.responseText);
+            error: function(err) {
+                $(".resultDiv").empty();
+                $(".resultDiv").append(err.responseText);
             }
         });
 
@@ -74,44 +74,48 @@ $(function() {
 
     $("#getAllProjectSubmit").on("click", function(e) {
         e.preventDefault();
-            $.ajax({
-            type:"get",
-            url:"/api/getallproject/",
-            success: function(response){
-              console.log(response);
-              $(".resultDiv").empty();
-              var ResultDiv = $("<div>");
-              response.map(function(value,index){
-              ResultDiv.append($("<div>").css({border:"1px solid black"}).append($("<span>").text(value.project_name)).append($("<span>").text(value._id)));
-              })
-              $(".resultDiv").append(ResultDiv);
+        $.ajax({
+            type: "get",
+            url: "/api/getallproject/",
+            success: function(response) {
+                console.log(response);
+                $(".resultDiv").empty();
+                var ResultDiv = $("<div>");
+                response.map(function(value, index) {
+                    ResultDiv.append($("<div>").css({
+                        border: "1px solid black"
+                    }).append($("<span>").text(value.project_name)).append($("<span>").text(value._id)));
+                })
+                $(".resultDiv").append(ResultDiv);
             },
-                          error:function(err){
-              $(".resultDiv").empty();
-               $(".resultDiv").append(err.responseText);
+            error: function(err) {
+                $(".resultDiv").empty();
+                $(".resultDiv").append(err.responseText);
             }
         });
 
     });
-  
-      $("#issueGetSubmit").on("click", function(e) {
+
+    $("#issueGetSubmit").on("click", function(e) {
         e.preventDefault();
-            $.ajax({
-            type:"get",
-            data:$('#issueGetForm').serialize(),
-            url:"/api/getallissue/",
-            success: function(response){
-              console.log(response);
-              $(".resultDiv").empty();
-              var ResultDiv = $("<div>");
-              response.map(function(value,index){
-              ResultDiv.append($("<div>").css({border:"1px solid black"}).append($("<span>").text(value.project_name)).append($("<span>").text(value._id)));
-              })
-              $(".resultDiv").append(ResultDiv);
+        $.ajax({
+            type: "get",
+            data: $('#issueGetForm').serialize(),
+            url: "/api/getallissue/",
+            success: function(response) {
+                console.log(response);
+                $(".resultDiv").empty();
+                var ResultDiv = $("<div>");
+                response.map(function(value, index) {
+                    ResultDiv.append($("<div>").css({
+                        border: "1px solid black"
+                    }).append($("<span>").text(value.project_name)).append($("<span>").text(value._id)));
+                })
+                $(".resultDiv").append(ResultDiv);
             },
-            error:function(err){
-              $(".resultDiv").empty();
-               $(".resultDiv").append(err.responseText);
+            error: function(err) {
+                $(".resultDiv").empty();
+                $(".resultDiv").append(err.responseText);
             }
         });
 
--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -6,7 +6,7 @@ exports.createProject = function(req, res) {
     let project = new projectModel({
         project_name: req.body.projectName
     });
-    projectModel.createProject(project,function(err, data) {
+    projectModel.createProject(project, function(err, data) {
         if (err) console.log(err);
         else {
             return res.send(req.body.projectName + "has been created and id of this projetc is :" + data._id);
@@ -25,38 +25,37 @@ exports.getAllProject = function(req, res) {
 }
 
 exports.getAllIssue = function(req, res) {
-  projectModel.getProjectByName(req.query.ProjectName,function(err,data){
-    console.log(data);
-    console.log(1);
-  if(data===null){
-  return res.status(500).send("No such project");
-  }
-    else{
-          issueModel.getAllIssue(data._id,function(err, data) {
-        if (err)
-            console.log(err);
-        else {
-            return res.json(data)
+    projectModel.getProjectByName(req.query.ProjectName, function(err, data) {
+        console.log(data);
+        console.log(1);
+        if (data === null) {
+            return res.status(500).send("No such project");
+        } else {
+            issueModel.getAllIssue(data._id, function(err, data) {
+                if (err)
+                    console.log(err);
+                else {
+                    return res.json(data)
+                }
+            });
         }
+
     });
-    }
-  
-  });
 
 }
 
 exports.createIssue = function(req, res) {
     let projectId;
     projectModel.getProjectByName(req.params.projectName, function(err, data) {
-        if (data===null) {
+        if (data === null) {
             let project = new projectModel({
                 project_name: req.params.projectName
             });
-            projectModel.createProject(project,function(err, data) {
+            projectModel.createProject(project, function(err, data) {
                 if (err) console.log(err);
                 else {
                     projectId = data._id;
-                   console.log(projectId);
+                    console.log(projectId);
                     let issue = new issueModel({
                         issue_title: req.body.issueName,
                         issue_text: req.body.issueText,
@@ -69,7 +68,7 @@ exports.createIssue = function(req, res) {
                         _project: projectId
                     });
 
-                    issueModel.createIssue(issue,function(err, data) {
+                    issueModel.createIssue(issue, function(err, data) {
                         if (err) console.log(err);
                         else {
                             return res.json(data);
@@ -91,7 +90,7 @@ exports.createIssue = function(req, res) {
                 status: req.body.status,
                 _project: projectId
             });
-            issueModel.createIssue(issue,function(err, data) {
+            issueModel.createIssue(issue, function(err, data) {
                 if (err) console.log(err);
                 else {
                     return res.json(data);
@@ -102,28 +101,28 @@ exports.createIssue = function(req, res) {
 
 }
 
-exports.deleteIssue = function (req,res){
-  console.log(req.body);
-if(req.body.IssueId==='')
-   return res.status(400).send("No Id Send");
-else
-  issueModel.deleteIssueById(req.body.IssueId,function(err,data){
-  if(err)
-     return res.status(400).send("No such issue");
+exports.deleteIssue = function(req, res) {
+    console.log(req.body);
+    if (req.body.IssueId === '')
+        return res.status(400).send("No Id Send");
     else
-      return res.status(200).send("issue deleted from project: "+req.body.ProjectName +"with ıd of :"+ req.body.IssueId);
-  });
+        issueModel.deleteIssueById(req.body.IssueId, function(err, data) {
+            if (err)
+                return res.status(400).send("No such issue");
+            else
+                return res.status(200).send("issue deleted from project: " + req.body.ProjectName + "with ıd of :" + req.body.IssueId);
+        });
 }
-exports.updateIssue = function (req,res){
-  console.log(req.body);
-  //findByIdAndUpdate
-if(req.body.issueId==='')
-  return res.status(400).send("No Id Send");
-//else
-  /*issueModel.updateIssueById(req.body.IssueId,function(err,data){
-  if(err)
-     return res.status(400).send("No such issue");
-    else
-      return res.status(200).send("issue deleted from project: "+req.body.ProjectName +"with ıd of :"+ req.body.IssueId);
-  });*/
+exports.updateIssue = function(req, res) {
+    console.log(req.body);
+    //findByIdAndUpdate
+    if (req.body.issueId === '')
+        return res.status(400).send("No Id Send");
+    //else
+    /*issueModel.updateIssueById(req.body.IssueId,function(err,data){
+    if(err)
+       return res.status(400).send("No such issue");
+      else
+        return res.status(200).send("issue deleted from project: "+req.body.ProjectName +"with ıd of :"+ req.body.IssueId);
+    });*/
 }
