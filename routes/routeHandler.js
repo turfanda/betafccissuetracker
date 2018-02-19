7992c2980b4719f49f1af0a51590dddac8221c49
Glitch (betafcc-issuetracker) <none>
2018-02-19 06:15:10 +0000

    🚦🍕 Checkpoint routes/routeHandler.js:596315/655

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -113,12 +113,36 @@ exports.deleteIssue = function(req, res) {
 }
 exports.updateIssue = function(req, res) {
     console.log(req.body);
+
     //findByIdAndUpdate
     if (req.body.issue_Id === '')
         return res.status(400).send("No Id Send");
 else{
+  let updates={}
+if(req.body.issue_title!==undefined&&req.body.issue_title!=="")
+  updates.issue_title=req.body.issue_title;
+  if(req.body.issue_text!==undefined&&req.body.issue_text!=="")
+  updates.issue_text=req.body.issue_text;
+  if(req.body.created_by!==undefined&&req.body.created_by!=="")
+  updates.created_by=req.body.created_by;
+  if(req.body.assigned_to!==undefined&&req.body.assigned_to!=="")
+  updates.assigned_to=req.body.assigned_to;
+  if(req.body.status!==undefined&&req.body.status!=="")
+  updates.status=req.body.status;
+  if(req.body.open!==undefined&&req.body.open!=="")
+  updates.open=req.body.open;
+if(Object.keys(updates).length !== 0)
+{updates.updated_on=Date().now;
+issueModel.updateIssueById(req.body.issue_Id,updates,function(err,data){
+    if(err)
+       return res.status(400).send("No such issue");
+      else
+        return res.status(200).send("issue updated");
+}
 
 }
+    }
+                           }
 /*    if()
   let updates=	{
   issue_title: {type: String},
