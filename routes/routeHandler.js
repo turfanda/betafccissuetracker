8158e610aa029df412ef9c24e3b6de899aaebeae
Glitch (betafcc-issuetracker) <none>
2018-02-19 06:55:44 +0000

    🍵🍨 Checkpoint routes/routeHandler.js:596315/28 views/index.html:596315/67 public/style.css:596315/153

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -25,10 +25,7 @@ exports.getAllProject = function(req, res) {
 }
 
 exports.getAllIssue = function(req, res) {
-  console.log(1);
-  console.log(req.query.project_name);
-  console.log(req.params);
-    projectModel.getProjectByName(req.query.project_name, function(err, data) {
+    projectModel.getProjectByName(req.params.project_name, function(err, data) {
         if (data === null) {
             return res.status(500).send("No such project");
         } else {
